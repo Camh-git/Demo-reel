df357524680023274bc39639607b42a444b40616
Camh-git <74473559+Camh-git@users.noreply.github.com>
2021-05-28 17:24:54 +0100

    Add files via upload

---
+++ C#/Overloading and multi return/Overloading and multi return/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Overloading_and_multi_return
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Run_overload = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Run_overload
+            // 
+            this.Run_overload.Location = new System.Drawing.Point(12, 74);
+            this.Run_overload.Name = "Run_overload";
+            this.Run_overload.Size = new System.Drawing.Size(120, 23);
+            this.Run_overload.TabIndex = 0;
+            this.Run_overload.Text = "Run overload";
+            this.Run_overload.UseVisualStyleBackColor = true;
+            this.Run_overload.Click += new System.EventHandler(this.Run_overload_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Run tupple";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Run_overload);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Run_overload;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ C#/Overloading and multi return/Overloading and multi return/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Overloading_and_multi_return
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        //Overloading, this is usefull as it allows mutliple functions with the same name, so a caller can call a function(such as add) with a variety of inputs that 
+        //will all be handled appropriatly and still give the same result(a more complex version might have multiple methods that format the inputs then call a processor method)
+        public double add (int a, int b)
+        {
+            double result = a + b;
+            return (result);
+        }
+        public double add (int a, double b)
+        {
+            double result = a + b;
+            return (result);
+        }
+        public double add (double a, double b)
+        {
+            double result = a + b;
+            return(result);
+        }
+
+        //tuples, allows a method to return more than one value
+        (double, string) Marked_add(int a, int b)
+        {
+            double result = a + b;
+            return (result, "method 1");
+        }
+        (double, string) Marked_add(int a, double b)
+        {
+            double result = a + b;
+            return (result, "method 2");
+        }
+        (double, string) Marked_add(double a, double b)
+        {
+            double result = a + b;
+            return (result,"method 3");
+        }
+
+
+        private void Run_overload_Click(object sender, EventArgs e)
+        {
+            double answer = add(5.1,7.2);
+            MessageBox.Show("Result: " + answer);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //note: the vars can be named, this lets you access them as answer.name
+            (double,string) answer = Marked_add(5,7.2);
+            MessageBox.Show("Result:" + answer.Item1 + " " + answer.Item2 + " was used.");
+        }
+        //to check if a dec is also an int: if (dec % 1 == 0)
+    }
+}
