95575a5223584d0bc9f88753c8bf5fa14bdab6f0
Camh-git <74473559+Camh-git@users.noreply.github.com>
2023-04-18 16:57:27 +0100

    Create Match_strings.py

---
+++ Python/Match_strings.py
@@ -0,0 +1,8 @@
+#A simple script to check if 2 strings match
+First_string = input("Please enter the first string:")
+Second_string = input("Please enter the Second string:")
+
+if(First_string == Second_string):
+    print("The strings match")
+else:
+    print("The strings are different")
