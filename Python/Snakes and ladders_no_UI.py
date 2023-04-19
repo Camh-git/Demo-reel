4270ef69b58163d83440005bba92e8416bdfdb10
Camh-git <74473559+Camh-git@users.noreply.github.com>
2023-04-19 18:47:43 +0100

    Update Snakes and ladders_no_UI.py
    Added the entry positions to the snake and ladder announcements
    changed the last ladder's entrance from 73 to 74 so that the ladder starting at 58 doesn't exit and immediately send players up another ladder

--- Python/Snakes and ladders_no_UI.py
+++ Python/Snakes and ladders_no_UI.py
@@ -1,17 +1,17 @@
-#This is a text based, self playing version of snakes and ladders
 import random
 
 class Jump():
   def __init__(self,entry,ext):
       self.Entrance = entry
       self.Exit = ext
- 
+
 def Roll():
   return(random.randrange(1,7))
 
+
 #setup
 Snakes = [Jump(43,12),Jump(72,52),Jump(13,8),Jump(52,25),Jump(90,61),Jump(98,20)]
-Ladders = [Jump(8,20),Jump(21,35),Jump(40,52),Jump(58,73),Jump(62,78),Jump(73,82)]
+Ladders = [Jump(8,20),Jump(21,35),Jump(40,52),Jump(58,73),Jump(62,78),Jump(74,82)]
 max_pos = 100
 pos=0
 
@@ -21,17 +21,19 @@ while pos <max_pos:
   if (pos + Score <= max_pos):
     pos += Score
   else:
-    print("You rolled:" + str(Score) +", unfortunatly this takes you over the end so you cant move.")
+    print("You rolled:" + str(Score) + ", unfortunatly this takes you over the end so you cant move.")
   print("Postion: " + str(pos))
   for x in Snakes:
     if pos == x.Entrance:     
       pos = x.Exit
-      print("Oh no, you hit a snake, you are now at:" + str(pos))
+      print("Oh no, you hit a snake at " + str(x.Entrance) +", you are now at " + str(pos))
   for x in Ladders:
     if pos == x.Entrance:      
       pos = x.Exit
-      print("Yay, you found a ladder!, you are now at: " + str(pos))
+      print("Yay, you found a ladder!, you have jumped from " + str(x.Entrance) + " to " + str(pos))
 
 print("Congratualtions, you won!")
-#for x in range(50):
- # print(Roll())
+
+#Changes:
+#Added entry postions to snake and ladder announcements
+#changed last ladder entrance to 74 to stop a player getting the 58 ladder doubling up and landing on 82
