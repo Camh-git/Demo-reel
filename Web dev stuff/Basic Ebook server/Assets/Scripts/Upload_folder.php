68a6e5eee1daa9a417246e2ab7b8793ff273d095
Camh-git <74473559+Camh-git@users.noreply.github.com>
2023-05-22 22:30:24 +0100

    Update Upload_folder.php
    Updated input variables to work with new naming scheme, still very WIP

--- Web dev stuff/Basic Ebook server/Assets/Scripts/Upload_folder.php
+++ Web dev stuff/Basic Ebook server/Assets/Scripts/Upload_folder.php
@@ -2,27 +2,33 @@
     //$files = array_filter($_FILES['inpFiles'],$_POST['UF_name']); 
 
     //Create a folder for the books, using the name the user provided
-    $folder = "../../Books/";
-    mkdir($folder);
+    //TODO: get new folder name, set up try catch
+    if(isset($_FILES['UF_inp_Files']))
+    {
+      $folder = "../../Books/";
+      mkdir($folder);
 
-    //upload the files
-    $total = count($_FILES['inpFiles']['name']);
-    for( $i=0 ; $i < $total ; $i++ ) {
+      //upload the files
+      $total = count($_FILES['UF_inp_Files']['name']);
+      for( $i=0 ; $i < $total ; $i++ ) {
 
         //Get the temp file path
-        $tmpFilePath = $_FILES['inpFiles']['tmp_name'][$i];
+        $tmpFilePath = $_FILES['UF_inp_Files']['tmp_name'][$i];
       
         //Make sure we have a file path
         if ($tmpFilePath != ""){
           //Setup our new file path
-          $newFilePath = $folder . $_FILES['inpFiles']['name'][$i];
+          $newFilePath = $folder . $_FILES['UF_inp_Files']['name'][$i];
       
           //Upload the file into the temp dir
           if(move_uploaded_file($tmpFilePath, $newFilePath)) {
-      
-            //Handle other code here
+            //Handle  here
       
           }
         }
       }
+    }
+    else{
+      echo("Input file not set, this is likely because the file is too large, the limit is ~30MB");
+    }
 ?>
