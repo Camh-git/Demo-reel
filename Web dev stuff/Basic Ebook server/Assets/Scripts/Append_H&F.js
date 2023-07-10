b9b6b4ef5eff7388a1cf25c98ed0e7ca34473f9d
Camh-git <74473559+Camh-git@users.noreply.github.com>
2023-07-10 18:35:52 +0100

    Update Append_H&F.js
    Fixed code styles, switched lets to consts and commented out the "everything worked" messages

--- Web dev stuff/Basic Ebook server/Assets/Scripts/Append_H&F.js
+++ Web dev stuff/Basic Ebook server/Assets/Scripts/Append_H&F.js
@@ -1,42 +1,60 @@
 //This is the functional version
-function getheader(){	
-	var header_container = document.getElementById("header_container");
-	var header_file = "Assets/Header.html";
-	if (header_file)
-	{
-		var xhttp = new XMLHttpRequest();		
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4) {
-				console.log("Header request state/status: ", this.readyState, this.status);
-				if (this.status == 200) {header_container.innerHTML = this.responseText;}
-				else if (this.status == 404) {header_container.innerHTML = "Page not found.";}
-				else {header_container.innerHTML ="Error fetching header.";}
-			}
-		}
-		xhttp.open("GET", header_file, true);			
-		xhttp.send();
-		return;						
-    }
+function getheader() {
+  const HEADER_CONTAINER = document.getElementById("header_container");
+  const HEADER_FILE = "Assets/Header.html";
+  if (HEADER_FILE) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        /*
+        console.log(
+          "Header request state/status: ",
+          this.readyState,
+          this.status
+        );
+        */
+        if (this.status == 200) {
+          HEADER_CONTAINER.innerHTML = this.responseText;
+        } else if (this.status == 404) {
+          HEADER_CONTAINER.innerHTML = "Page not found.";
+        } else {
+          HEADER_CONTAINER.innerHTML = "Error fetching header.";
+        }
+      }
+    };
+    xhttp.open("GET", HEADER_FILE, true);
+    xhttp.send();
+    return;
+  }
 }
 
-function getfooter(){
-	var footer_container = document.getElementById("footer_container");
-	var footer_file = "Assets/Footer.html";
-	if (footer_file)
-	{
-		var xhttp = new XMLHttpRequest();		
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4) {
-				console.log("Footer request state/status: ", this.readyState, this.status);
-				if (this.status == 200) {footer_container.innerHTML = this.responseText;}
-				else if (this.status == 404) {footer_container.innerHTML = "Page not found.";}
-				else {header_container.innerHTML ="Error fetching footer.";}
-			}
-		}
-		xhttp.open("GET", footer_file, true);
-		xhttp.send();
-		return;						
-    }
+function getfooter() {
+  var FOOTER_CONTAINER = document.getElementById("footer_container");
+  var FOOTER_FILE = "Assets/Footer.html";
+  if (FOOTER_FILE) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4) {
+        /*
+        console.log(
+          "Footer request state/status: ",
+          this.readyState,
+          this.status
+        );
+        */
+        if (this.status == 200) {
+          FOOTER_CONTAINER.innerHTML = this.responseText;
+        } else if (this.status == 404) {
+          FOOTER_CONTAINER.innerHTML = "Page not found.";
+        } else {
+          header_container.innerHTML = "Error fetching footer.";
+        }
+      }
+    };
+    xhttp.open("GET", FOOTER_FILE, true);
+    xhttp.send();
+    return;
+  }
 }
 
 getheader();
