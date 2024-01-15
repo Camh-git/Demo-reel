cd227be2a3c70b582a649c512c7f804f8bdb6fec
Camh-git <cameronhillier1@gmail.com>
2024-01-15 22:44:04 +0000

    Feat: edit and recalculate
    Added the ability to edit an entry in the "Runtimes" column and re-calculate all subsequent start/end times based on the new information.
    Converted the Format_as_date and Calculate_time_to_add code into their own functions for re-usability, rather than just being in a button.
    Protected data by making only columns the user needs access to editable.
    Did a bit of UI tidying.

--- C#/Chapter time calc/Chapter time calc/Form1.Designer.cs
+++ C#/Chapter time calc/Chapter time calc/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.Label label3;
             this.Timing_view = new System.Windows.Forms.DataGridView();
             this.Number = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.RunTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -37,8 +38,25 @@
             this.Time_input = new System.Windows.Forms.TextBox();
             this.Zero_index_check = new System.Windows.Forms.CheckBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.Recalculate_times = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Timing_view)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label3
+            // 
+            label3.Location = new System.Drawing.Point(1, 19);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(198, 64);
+            label3.TabIndex = 7;
+            label3.Text = "If you make a mistake simply change the \"Run time\" field for the chapter with the" +
+    " issue and press the \"Recalculate\" button.";
+            label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
             // Timing_view
             // 
@@ -48,7 +66,7 @@
             this.RunTime,
             this.StartTime,
             this.EndTime});
-            this.Timing_view.Location = new System.Drawing.Point(12, 12);
+            this.Timing_view.Location = new System.Drawing.Point(12, 10);
             this.Timing_view.Name = "Timing_view";
             this.Timing_view.RowTemplate.Height = 25;
             this.Timing_view.Size = new System.Drawing.Size(443, 937);
@@ -68,15 +86,17 @@
             // 
             this.StartTime.HeaderText = "Start time";
             this.StartTime.Name = "StartTime";
+            this.StartTime.ReadOnly = true;
             // 
             // EndTime
             // 
             this.EndTime.HeaderText = "End time";
             this.EndTime.Name = "EndTime";
+            this.EndTime.ReadOnly = true;
             // 
             // Add_time
             // 
-            this.Add_time.Location = new System.Drawing.Point(562, 162);
+            this.Add_time.Location = new System.Drawing.Point(54, 61);
             this.Add_time.Name = "Add_time";
             this.Add_time.Size = new System.Drawing.Size(75, 23);
             this.Add_time.TabIndex = 1;
@@ -86,7 +106,7 @@
             // 
             // Time_input
             // 
-            this.Time_input.Location = new System.Drawing.Point(548, 133);
+            this.Time_input.Location = new System.Drawing.Point(40, 32);
             this.Time_input.Name = "Time_input";
             this.Time_input.Size = new System.Drawing.Size(100, 23);
             this.Time_input.TabIndex = 2;
@@ -95,7 +115,7 @@
             // Zero_index_check
             // 
             this.Zero_index_check.AutoSize = true;
-            this.Zero_index_check.Location = new System.Drawing.Point(553, 217);
+            this.Zero_index_check.Location = new System.Drawing.Point(45, 120);
             this.Zero_index_check.Name = "Zero_index_check";
             this.Zero_index_check.Size = new System.Drawing.Size(95, 19);
             this.Zero_index_check.TabIndex = 3;
@@ -106,27 +126,71 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(562, 115);
+            this.label1.Location = new System.Drawing.Point(54, 14);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(61, 15);
             this.label1.TabIndex = 4;
             this.label1.Text = "Enter time";
+            // 
+            // Recalculate_times
+            // 
+            this.Recalculate_times.Location = new System.Drawing.Point(54, 91);
+            this.Recalculate_times.Name = "Recalculate_times";
+            this.Recalculate_times.Size = new System.Drawing.Size(75, 23);
+            this.Recalculate_times.TabIndex = 5;
+            this.Recalculate_times.Text = "Recalculate";
+            this.Recalculate_times.UseVisualStyleBackColor = true;
+            this.Recalculate_times.Click += new System.EventHandler(this.Recalculate_times_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.Time_input);
+            this.groupBox1.Controls.Add(this.Add_time);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.Zero_index_check);
+            this.groupBox1.Location = new System.Drawing.Point(504, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 160);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(157, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "(Select before inputing data)";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Recalculate_times);
+            this.groupBox2.Controls.Add(label3);
+            this.groupBox2.Location = new System.Drawing.Point(504, 210);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 120);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Editing";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 961);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.Zero_index_check);
-            this.Controls.Add(this.Time_input);
-            this.Controls.Add(this.Add_time);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Timing_view);
             this.Name = "Form1";
             this.Text = "Chapter time calculator";
             ((System.ComponentModel.ISupportInitialize)(this.Timing_view)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -136,10 +200,14 @@
         private Button Add_time;
         private TextBox Time_input;
         private CheckBox Zero_index_check;
+        private Label label1;
+        private Button Recalculate_times;
+        private GroupBox groupBox1;
+        private Label label2;
+        private GroupBox groupBox2;
         private DataGridViewTextBoxColumn Number;
         private DataGridViewTextBoxColumn RunTime;
         private DataGridViewTextBoxColumn StartTime;
         private DataGridViewTextBoxColumn EndTime;
-        private Label label1;
     }
 }
--- C#/Chapter time calc/Chapter time calc/Form1.cs
+++ C#/Chapter time calc/Chapter time calc/Form1.cs
@@ -16,62 +16,73 @@ namespace Chapter_time_calc
             InitializeComponent();
         }
 
+        private string Format_as_date(string input)
+        {
+            string formatted_input = "";
+            string[] sections = input.Split(':');
+            for (int i =0; i < sections.Length; i++)
+            {
+                if (sections[i].Length == 1)
+                {
+                    sections[i] = string.Format("0{0}", sections[i]);
+                }
+                formatted_input += string.Format("{0}:", sections[i]);
+            }
+            formatted_input = formatted_input.Remove(formatted_input.Length - 1);
+            return formatted_input;
+        }
+
+        private int Calculate_time_to_add(string input)
+        {
+            int time_to_add = 0;
+            string[] sections = input.Split(':');
+            switch (sections.Count())
+            {
+                case 1:
+                    if (Convert.ToInt32(sections[0]) > 59)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    time_to_add += Convert.ToInt32(sections[0]);
+                    break;
+                case 2:
+                    if (Convert.ToInt32(sections[0]) > 59 || Convert.ToInt32(sections[1]) > 59)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    time_to_add += Convert.ToInt32(sections[0]) * 60;
+                    time_to_add += Convert.ToInt32(sections[1]);
+                    break;
+                case 3:
+                    if (Convert.ToInt32(sections[0]) > 23 || Convert.ToInt32(sections[1]) > 59 || Convert.ToInt32(sections[2]) > 59)
+                    {
+                        throw new ArgumentOutOfRangeException();
+                    }
+                    time_to_add += Convert.ToInt32(sections[0]) * 60 * 60;
+                    time_to_add += Convert.ToInt32(sections[1]) * 60;
+                    time_to_add += Convert.ToInt32(sections[2]);
+                    break;
+                default:
+                    MessageBox.Show("Error: there appear to be more than 2 or 0 : seperating the time segments", "Invalid input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return 0;
+            }
+            return time_to_add;
+        }
+
+        //TODO: add json save and load,
         private void Add_time_Click(object sender, EventArgs e)
         {
             try
             {
-                int time_to_add = 0;
-                string[] sections = Time_input.Text.Split(':');
-
-
-                //Format input for display
-                string formatted_input = "";
-                for (int i =0; i < sections.Length; i++)
-                {
-                    if (sections[i].Length == 1)
-                    {
-                        sections[i] =string.Format("0{0}", sections[i]);
-                    }
-                    formatted_input += string.Format("{0}:",sections[i]);
-                }
-                formatted_input = formatted_input.Remove(formatted_input.Length - 1);
-
-                //Calculate the amount of time to add
-                switch (sections.Length)
-                {
-                    case 1:
-                        if (Convert.ToInt32(sections[0])>59)
-                        {
-                            throw new ArgumentOutOfRangeException();
-                        }
-                        time_to_add += Convert.ToInt32(sections[0]);
-                        break;
-                    case 2:
-                        if (Convert.ToInt32(sections[0]) > 59 || Convert.ToInt32(sections[1]) > 59)
-                        {
-                            throw new ArgumentOutOfRangeException();
-                        }
-                        time_to_add += Convert.ToInt32(sections[0])*60;
-                        time_to_add += Convert.ToInt32(sections[1]);
-                        break;
-                    case 3:
-                        if (Convert.ToInt32(sections[0]) > 23 || Convert.ToInt32(sections[1]) > 59 || Convert.ToInt32(sections[2]) >59)
-                        {
-                            throw new ArgumentOutOfRangeException();
-                        }
-                        time_to_add += Convert.ToInt32(sections[0]) * 60 * 60;
-                        time_to_add += Convert.ToInt32(sections[1]) * 60;
-                        time_to_add += Convert.ToInt32(sections[2]);
-                        break;
-                    default:
-                        MessageBox.Show("Error: there appear to be more than 2 or 0 : seperating the time segments","Invalid input",MessageBoxButtons.OK,MessageBoxIcon.Error);
-                        return;
-                }
-
+                //Format input for display, Calculate the amount of time to add (can you tell this section was a lot longer before these became their own functions?)
+                string formatted_input = Format_as_date(Time_input.Text);
+                int time_to_add = Calculate_time_to_add(formatted_input);
+            
                 //create new row with the index, formatted input as runtime, total time as start time and totalTime+time_to_add as the end time
                 Timing_view.Rows.Add(index, formatted_input, TimeSpan.FromSeconds(TotalTime), TimeSpan.FromSeconds(TotalTime + time_to_add));
                 TotalTime += time_to_add;
                 index++;
+                Time_input.Text = "";
             }
             catch
             {
@@ -106,6 +117,30 @@ namespace Chapter_time_calc
                 MessageBox.Show("Cannot revert indexing after data has been added, clear data or restart to change indexing.","Index error", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
            
-        }        
+        }
+
+        private void Recalculate_times_Click(object sender, EventArgs e)
+        {
+            int running_time = 0;
+            foreach( DataGridViewRow row in Timing_view.Rows)
+            {
+                if (row.Cells[0].Value != null)
+                {
+                    //Make sure any changes the user made are properly formatted.
+                    row.Cells[1].Value = Format_as_date(row.Cells[1].Value.ToString());
+                    row.Cells[2].Value = Format_as_date(row.Cells[2].Value.ToString());
+                    row.Cells[3].Value = Format_as_date(row.Cells[3].Value.ToString());
+
+                    //Calculate at display the new values
+                    int time_to_add = Calculate_time_to_add(row.Cells[1].Value.ToString());
+                    row.Cells[2].Value = Format_as_date(TimeSpan.FromSeconds(running_time).ToString());
+                    row.Cells[3].Value = Format_as_date(TimeSpan.FromSeconds((running_time + time_to_add)).ToString());
+                    running_time += time_to_add;
+                    TotalTime = running_time;
+                }
+
+            }
+            MessageBox.Show(TotalTime.ToString());
+        }
     }
 }
