6ff7622a19a14e40b14e8830464ec98d9bc96ccb
Camh-git <cameronhillier1@gmail.com>
2024-01-16 20:48:16 +0000

    Feat: JSON save and load
    Added the ability to copy the timing data into a JSON file for later use/ storage, along with the ability to load JSON files in the save functions format into the data grid view to be viewed/ amended.
    made elementes that could be static static.
    Added warning message to recalculate_times_click, refactored the method's checks.
    Removed extraneous message boxes.
    Added reset button

--- C#/Chapter time calc/Chapter time calc/Form1.Designer.cs
+++ C#/Chapter time calc/Chapter time calc/Form1.Designer.cs
@@ -42,10 +42,19 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Load_file_btn = new System.Windows.Forms.Button();
+            this.Save_name_input = new System.Windows.Forms.TextBox();
+            this.Save_file_btn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
             label3 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Timing_view)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // label3
@@ -169,18 +178,89 @@
             // 
             this.groupBox2.Controls.Add(this.Recalculate_times);
             this.groupBox2.Controls.Add(label3);
-            this.groupBox2.Location = new System.Drawing.Point(504, 210);
+            this.groupBox2.Location = new System.Drawing.Point(504, 204);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(200, 120);
             this.groupBox2.TabIndex = 8;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Editing";
             // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.Load_file_btn);
+            this.groupBox3.Controls.Add(this.Save_name_input);
+            this.groupBox3.Controls.Add(this.Save_file_btn);
+            this.groupBox3.Controls.Add(this.label5);
+            this.groupBox3.Location = new System.Drawing.Point(503, 355);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(200, 160);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Save/ load";
+            // 
+            // Load_file_btn
+            // 
+            this.Load_file_btn.Location = new System.Drawing.Point(55, 33);
+            this.Load_file_btn.Name = "Load_file_btn";
+            this.Load_file_btn.Size = new System.Drawing.Size(75, 23);
+            this.Load_file_btn.TabIndex = 5;
+            this.Load_file_btn.Text = "Load";
+            this.Load_file_btn.UseVisualStyleBackColor = true;
+            this.Load_file_btn.Click += new System.EventHandler(this.Load_file_btn_Click);
+            // 
+            // Save_name_input
+            // 
+            this.Save_name_input.Location = new System.Drawing.Point(41, 102);
+            this.Save_name_input.Name = "Save_name_input";
+            this.Save_name_input.Size = new System.Drawing.Size(100, 23);
+            this.Save_name_input.TabIndex = 2;
+            // 
+            // Save_file_btn
+            // 
+            this.Save_file_btn.Location = new System.Drawing.Point(55, 131);
+            this.Save_file_btn.Name = "Save_file_btn";
+            this.Save_file_btn.Size = new System.Drawing.Size(75, 23);
+            this.Save_file_btn.TabIndex = 1;
+            this.Save_file_btn.Text = "Save";
+            this.Save_file_btn.UseVisualStyleBackColor = true;
+            this.Save_file_btn.Click += new System.EventHandler(this.Save_file_btn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 15);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Enter save file name:";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Location = new System.Drawing.Point(503, 536);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(200, 100);
+            this.groupBox4.TabIndex = 10;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Reset";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Reset";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 961);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.Timing_view);
@@ -190,6 +270,9 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -209,5 +292,12 @@
         private DataGridViewTextBoxColumn RunTime;
         private DataGridViewTextBoxColumn StartTime;
         private DataGridViewTextBoxColumn EndTime;
+        private GroupBox groupBox3;
+        private Button Load_file_btn;
+        private TextBox Save_name_input;
+        private Button Save_file_btn;
+        private Label label5;
+        private GroupBox groupBox4;
+        private Button button1;
     }
 }
--- C#/Chapter time calc/Chapter time calc/Form1.cs
+++ C#/Chapter time calc/Chapter time calc/Form1.cs
@@ -4,6 +4,9 @@ using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Windows.Forms;
 
 namespace Chapter_time_calc
 {
@@ -16,7 +19,7 @@ namespace Chapter_time_calc
             InitializeComponent();
         }
 
-        private string Format_as_date(string input)
+        private static string Format_as_time(string input)
         {
             string formatted_input = "";
             string[] sections = input.Split(':');
@@ -32,7 +35,7 @@ namespace Chapter_time_calc
             return formatted_input;
         }
 
-        private int Calculate_time_to_add(string input)
+        private static int  Calculate_time_to_add(string input)
         {
             int time_to_add = 0;
             string[] sections = input.Split(':');
@@ -69,13 +72,12 @@ namespace Chapter_time_calc
             return time_to_add;
         }
 
-        //TODO: add json save and load,
         private void Add_time_Click(object sender, EventArgs e)
         {
             try
             {
                 //Format input for display, Calculate the amount of time to add (can you tell this section was a lot longer before these became their own functions?)
-                string formatted_input = Format_as_date(Time_input.Text);
+                string formatted_input = Format_as_time(Time_input.Text);
                 int time_to_add = Calculate_time_to_add(formatted_input);
             
                 //create new row with the index, formatted input as runtime, total time as start time and totalTime+time_to_add as the end time
@@ -121,26 +123,117 @@ namespace Chapter_time_calc
 
         private void Recalculate_times_Click(object sender, EventArgs e)
         {
-            int running_time = 0;
-            foreach( DataGridViewRow row in Timing_view.Rows)
+            DialogResult confirm_recalc = MessageBox.Show("If you have applied all the runtime changes you want to make, press ok to continue","Recalculate",MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (confirm_recalc == DialogResult.OK)
             {
-                if (row.Cells[0].Value != null)
+                int running_time = 0;
+                foreach (DataGridViewRow row in Timing_view.Rows)
                 {
-                    //Make sure any changes the user made are properly formatted.
-                    row.Cells[1].Value = Format_as_date(row.Cells[1].Value.ToString());
-                    row.Cells[2].Value = Format_as_date(row.Cells[2].Value.ToString());
-                    row.Cells[3].Value = Format_as_date(row.Cells[3].Value.ToString());
+                    if (row.Cells[0].Value != null)
+                    {
+                        //Make sure any changes the user made are properly formatted.
+                        row.Cells[1].Value = Format_as_time(row.Cells[1].Value.ToString());
+                        row.Cells[2].Value = Format_as_time(row.Cells[2].Value.ToString());
+                        row.Cells[3].Value = Format_as_time(row.Cells[3].Value.ToString());
 
-                    //Calculate at display the new values
-                    int time_to_add = Calculate_time_to_add(row.Cells[1].Value.ToString());
-                    row.Cells[2].Value = Format_as_date(TimeSpan.FromSeconds(running_time).ToString());
-                    row.Cells[3].Value = Format_as_date(TimeSpan.FromSeconds((running_time + time_to_add)).ToString());
-                    running_time += time_to_add;
-                    TotalTime = running_time;
+                        //Calculate at display the new values
+                        int time_to_add = Calculate_time_to_add(row.Cells[1].Value.ToString());
+                        row.Cells[2].Value = Format_as_time(TimeSpan.FromSeconds(running_time).ToString());
+                        row.Cells[3].Value = Format_as_time(TimeSpan.FromSeconds((running_time + time_to_add)).ToString());
+                        running_time += time_to_add;
+                        TotalTime = running_time;
+                    }
+
+                }
+                MessageBox.Show("New total time (Seconds): " + TotalTime.ToString());
+            }
+        }
+
+        private void Load_file_btn_Click(object sender, EventArgs e)
+        {
+            DialogResult confirm_recalc = MessageBox.Show("This will overwrite any existing data view content, are you sure you want to continue?", "WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (confirm_recalc == DialogResult.Yes)
+            {
+
+                //Select the file
+                OpenFileDialog OFD = new OpenFileDialog();
+                OFD.Title = "";
+                OFD.Filter = "JSON file|*.json|All files|*.*";
+                OFD.InitialDirectory = String.Format("{0}../../../../Saved_JSON/", Directory.GetCurrentDirectory());
+                if (OFD.ShowDialog() == DialogResult.OK)
+                {
+                    //Setup by removing any existing content, set save target to this file
+                    string [] NameSegments = OFD.FileName.Split("\\");
+                    Save_name_input.Text = NameSegments[NameSegments.Length - 1].Split(".")[0];
+                    Timing_view.Rows.Clear();
+
+                    try
+                    {
+                        //Open and parse the file
+                        using (StreamReader file = File.OpenText(OFD.FileName))
+                        {
+                            using (JsonTextReader reader = new JsonTextReader(file))
+                            {
+                                JObject data = (JObject)JToken.ReadFrom(reader);
+                                for (int i = 0; i < data["Chapters"].Count(); i++)
+                                {
+                                    Timing_view.Rows.Add(i, data["Chapters"][i]["Runtime"], data["Chapters"][i]["Start_time"], data["Chapters"][i]["End_time"]);
+                                }
+                                index = data["Chapters"].Count();
+                                TotalTime = Calculate_time_to_add(data["Chapters"][data["Chapters"].Count()-1]["End_time"].ToString());
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Failed to load json file", "CRITICAL ERROR",
+                                MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                }
+            }
+        }
+
+        private void Save_file_btn_Click(object sender, EventArgs e)
+        {
+            string path = String.Format("{0}../../../../Saved_JSON/{1}.Json",Directory.GetCurrentDirectory(), Save_name_input.Text);
+            using (FileStream fs = File.Create(path))
+            {
+                //Populate the data
+                string data = "{\"Chapters\":[";
+                foreach (DataGridViewRow row in Timing_view.Rows)
+                {
+                    if (row.Cells[0].Value != null) //Prevents the currently empty new index from being saved
+                    {
+                        data += string.Format("{{\"Index\":\"{0}\",\"Runtime\":\"{1}\",\"Start_time\":\"{2}\",\"End_time\":\"{3}\"}},", row.Cells[0].Value, row.Cells[1].Value, row.Cells[2].Value, row.Cells[3].Value);
+                    }
+                }
+
+                data = data.Remove(data.Length - 1);
+                data += "]}";
+                try
+                {
+                    //write the data to the file
+                    byte[] info = new UTF8Encoding(true).GetBytes(data);
+                    fs.Write(info, 0, info.Length);
+                    MessageBox.Show("File saved successfully", "Saved", MessageBoxButtons.OK);
+                }
+                catch
+                {
+                    MessageBox.Show("Failed to save, please ensure that there are no other files with the same name","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
                 }
 
             }
-            MessageBox.Show(TotalTime.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult confirm_restart = MessageBox.Show("All unsaved data will be lost, continue?", "WARNING", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (confirm_restart == DialogResult.Yes)
+            {
+                Application.Restart();
+                Environment.Exit(0);
+            }
         }
     }
 }
